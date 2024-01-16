a85ce6e0a63ec3c0957588716a8006843321cc86
KSI5 <krissigebs@gmail.com>
2024-01-16 08:00:17 +0000

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
 """
-Minimum Operations Module
+Calculates the fewest number of operations needed to get 'n' characters of 'H'
 """
+
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
 
     Args:
-        n (int): Number of H characters to achieve.
-
-    Returns:
-        int: Fewest number of operations needed.
-
-    Notes:
-        If n is impossible to achieve, return 0.
+        n: input value
+        factor_list: List to save the operations
+    Return: the sum of the operations
     """
-    if n <= 1:
+    if n < 2:
         return 0
-
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
-
-if __name__ == "__main__":
-    # Example usage
-    n1 = 4
-    print("Min # of operations to reach {} char: {}".format(n1, minOperations(n1)))
-
-    n2 = 12
-    print("Min # of operations to reach {} char: {}".format(n2, minOperations(n2)))
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
