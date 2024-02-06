5e17db7dda6e8f50b78ab70ede225f297142787b
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-06 10:45:56 +0200

    Updated 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,85 +1,54 @@
 #!/usr/bin/python3
-"""
-Solution to the nqueens problem
-"""
+"""Solving N Queens with Backtracing"""
 import sys
 
 
-def print_solution(board):
+def nqueens(n, y, board):
     """
-    Print the N Queens solution
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
     """
-    solution = []
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] == 1:
-                solution.append([row, col])
-    print(solution)
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
-def is_safe(board, row, col):
-    """
-    Check if placing a queen at board[row][col] is safe
-    """
-    # Check the row on the left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens_util(board, col):
-    """
-    Utility function to solve N Queens problem using backtracking
-    """
-    n = len(board)
-
-    if col == n:
-        print_solution(board)
-        return
-
-    for i in range(n):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            solve_nqueens_util(board, col + 1)
-            board[i][col] = 0
-
-
-def solve_nqueens(n):
-    """
-    Solve the N Queens problem and print all solutions
-    """
-    if not isinstance(n, int):
-        print("N must be a number")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         sys.exit(1)
-
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0] * n for _ in range(n)]
-    solve_nqueens_util(board, 0)
+    nqueens(n, 0, [])
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-        solve_nqueens(n)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+if __name__ == '__main__':
+    main()
