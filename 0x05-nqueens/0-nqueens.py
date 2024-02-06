6687f444de8c6626f458a2269d3f912a1301597b
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-06 10:37:35 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
-""" N queens """
+"""
+N Queens problem
+"""
 import sys
 
+def print_solution(board):
+    """Print the chessboard"""
+    for row in board:
+        print(row)
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def is_safe(board, row, col, n):
+    """Check if it's safe to place a queen at board[row][col]"""
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+def solve_nqueens(board, row, n):
+    """Solve the N Queens problem using backtracking"""
+    if row == n:
+        print_solution([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+def main():
+    """Main function"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-n = int(sys.argv[1])
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
 
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+if __name__ == "__main__":
+    main()
